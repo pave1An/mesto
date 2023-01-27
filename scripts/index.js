866c7430258b0a2ef31b8c9545e47b78e55ef6f5
Pavel Anisimov <pavel13.88@yandex.ru>
2023-01-27 10:55:59 +0300

    feat: добавить плавное открытие popup

--- scripts/index.js
+++ scripts/index.js
@@ -26,25 +26,26 @@ const initialCards = [
 ];
 
 const profileElement = document.querySelector('.profile');
-const profileEditBtn = profileElement.querySelector('.profile__edit-btn');
 const profileName = profileElement.querySelector('.profile__name');
 const profileJob = profileElement.querySelector('.profile__job');
 
 const popupProfileForm = document.querySelector('.popup__profile-form');
-const closeProfileFormBtn = popupProfileForm.querySelector('.popup__close-btn');
 
 const profileForm = popupProfileForm.querySelector('.popup__form');
 const profileNameInput = profileForm.querySelector('.popup__input-text_type_name');
 const profileJobInput = profileForm.querySelector('.popup__input-text_type_job');
 
-const addCardBtn = profileElement.querySelector('.profile__add-btn');
-const cardForm = document.querySelector('.popup__card-form');
-const closeCardFormBtn = cardForm.querySelector('.popup__close-btn');
-const cardImageNameInput = cardForm.querySelector('.popup__input-text_type_name');
-const cardImageLinkInput = cardForm.querySelector('.popup__input-text_type_src');
+function switchPopup(popup) {
+  popup.classList.toggle('popup_opened');
+}
 
-const template = document.querySelector('.template')
-const photoList = document.querySelector('.photo-grid__list');
+function openProfileForm() {
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+  switchPopup(popupProfileForm);
+}
+const profileEditBtn = profileElement.querySelector('.profile__edit-btn');
+profileEditBtn.addEventListener ('click', openProfileForm);
 
 function submitProfileForm(evt) {
   evt.preventDefault();
@@ -52,58 +53,80 @@ function submitProfileForm(evt) {
   profileJob.textContent = profileJobInput.value;
   switchPopup(popupProfileForm);
 }
+profileForm.addEventListener ('submit', submitProfileForm);
+
+const closeProfileFormBtn = popupProfileForm.querySelector('.popup__close-btn');
+closeProfileFormBtn.addEventListener ('click', switchPopup.bind(null, popupProfileForm));
+
+const removeCard = function (elem) {
+  return () => elem.parentElement.remove();
+}
+
+const like = function (elem) {
+  return  () => elem.classList.toggle('element__like_active');
+}
+
+const photoList = document.querySelector('.photo-grid__list');
+const template = document.querySelector('.template')
+
+function createCard(name, link) {
+  const card = template.content.querySelector('.element').cloneNode(true);
+  const likeBtn = card.querySelector('.element__like');
+  const image = card.querySelector('.element__image');
+  const imageName = card.querySelector('.element__title');
+  const trashBtn = card.querySelector('.element__trash');
+
+  imageName.textContent = name;
+  image.setAttribute('src', link);
+  likeBtn.addEventListener('click', like(likeBtn));
+  trashBtn.addEventListener('click', removeCard(trashBtn));
+  image.addEventListener('click', openPopupImage(name, link));
+
+  return card;
+}
+
+function renderCard(name, link, container) {
+  container.prepend(createCard(name, link));
+}
+
+initialCards.forEach(element => renderCard(element.name, element.link, photoList));
+
+const cardForm = document.querySelector('.popup__card-form');
+const cardImageNameInput = cardForm.querySelector('.popup__input-text_type_name');
+const cardImageLinkInput = cardForm.querySelector('.popup__input-text_type_src');
+const addCardBtn = profileElement.querySelector('.profile__add-btn');
+addCardBtn.addEventListener('click', switchPopup.bind(null, cardForm));
 
 function submitCardForm(evt) {
   evt.preventDefault();
   const imageName = cardImageNameInput.value;
   const imageLink = cardImageLinkInput.value;
-  renderCard(imageName, imageLink);
+  renderCard(imageName, imageLink, photoList);
   switchPopup(cardForm);
   evt.target.reset();
 }
+cardForm.addEventListener('submit', submitCardForm);
 
 function closeCardForm() {
   cardImageNameInput.value = null;
   cardImageLinkInput.value = null;
   switchPopup(cardForm);
 }
-
-function openProfileForm() {
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
-  popupProfileForm.classList.toggle('popup_opened');
-}
-
-function switchPopup(popup) {
-  popup.classList.toggle('popup_opened');
-}
-
-function createCard(name, link) {
-  const card = template.content.querySelector('.element').cloneNode(true);
-  const imageName = card.querySelector('.element__title');
-  const imageLink = card.querySelector('.element__image');
-  const likeBtn = card.querySelector('.element__like');
-  const trashBtn = card.querySelector('.element__trash');
-
-  imageName.textContent = name;
-  imageLink.setAttribute('src', link);
-  likeBtn.addEventListener('click', () => likeBtn.classList.toggle('element__like_active'));
-  trashBtn.addEventListener('click', () => trashBtn.parentElement.remove());
-
-  return card;
-}
-
-function renderCard(name, link) {
-  photoList.prepend(createCard(name, link));
-}
-
-initialCards.forEach(element => renderCard(element.name, element.link));
-
-profileEditBtn.addEventListener ('click', openProfileForm);
-addCardBtn.addEventListener('click', switchPopup.bind(null, cardForm));
-
-closeProfileFormBtn.addEventListener ('click', switchPopup.bind(null, popupProfileForm));
-profileForm.addEventListener ('submit', submitProfileForm);
-
+const closeCardFormBtn = cardForm.querySelector('.popup__close-btn');
 closeCardFormBtn.addEventListener('click', closeCardForm);
-cardForm.addEventListener('submit', submitCardForm);
+
+const popupImage = document.querySelector('.popup__image');
+const closePopupImageBtn = popupImage.querySelector('.popup__close-btn');
+const popupImageView = popupImage.querySelector('.popup__image-view');
+const popupImageName = popupImage.querySelector('.popup__image-title');
+
+function openPopupImage(name,link) {
+  return () => {
+    popupImageView.setAttribute('src', link);
+    popupImageName.textContent = name;
+    switchPopup(popupImage);
+    popupImage.classList.add('popup_background_dark');
+  }
+}
+
+closePopupImageBtn.addEventListener('click', switchPopup.bind(null, popupImage));
